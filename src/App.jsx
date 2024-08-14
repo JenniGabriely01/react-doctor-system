92864f664be47f06f92822654c4322c06fc4368c
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-08-14 14:51:59 -0300

    Deixa as rotas funcionais

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/home';
+import Login from './pages/Login/login';
 import './global.css';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/Home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
 }
-
 export default App;
---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+    return (
+        <section>
+            <h2>Página de Login</h2>
+            <Link to="/Home">
+               Home
+            </Link>
+        </section>
+    )
+}
