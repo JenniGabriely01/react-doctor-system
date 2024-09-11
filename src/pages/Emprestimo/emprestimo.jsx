4ea635fad176afa25f35ace49bec519b1ba77d01
NathaliaMarchini <nathaliamarchini8@gmail.com>
2024-09-11 15:47:29 -0300

    Página de empréstimo

--- src/pages/Emprestimo/emprestimo.jsx
+++ src/pages/Emprestimo/emprestimo.jsx
@@ -1,7 +1,71 @@
-export default function Emprestimo () {
+import React, { useState } from 'react';
+import './emprestimo.css'
+import CadButton from '../../components/cadButtons/cadButtons';
+import imagemDireita from '../../assets/imagens/Emprestimo-img.png';
+import logoVertical from '../../assets/imagens/logoVertical.svg';
+import Input from '../../components/input/inpux'
+
+export default function Emprestimo() {
     return (
         <>
-         <h1>Empréstimo</h1>
+            <section className="Emprestimo">
+                <div className="container-Emprestimo">
+                    <div className="conteudo-Emprestimo">
+                        <h1 className='title'>Empréstimo</h1>
+                        <p className='p-Title'>Insira as informações do cliente.</p>
+                        <form action="">
+                            <Input
+                                placeholder="Cliente"
+                                type="text"
+                            />
+
+                            <Input
+                                placeholder="E-mail"
+                                type="text"
+                            />
+
+                            <Input
+                                placeholder="E-mail"
+                                type="text"
+                            />
+
+                            <Input
+                                placeholder="Telefone"
+                                type="text"
+                            />
+
+                            <Input
+                                placeholder="Nome do livro"
+                                type="text"
+                            />
+
+                            <Input
+                                placeholder="Data do Empréstimo"
+                                type="text"
+                            />
+
+                            <div className='buttons'>
+                                <CadButton
+                                    legendaBotao="Inserir"
+                                    margem="0 0 0 0"
+                                />
+                                <CadButton
+                                    legendaBotao="Voltar"
+                                    cor="50%"
+                                    margem="0 0 0 5%"
+                                    rota="/Clientes"
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className='img-Emprestimo'>
+                    <img className="background-imagem" src={imagemDireita} alt="Imagem lateral" />
+                    <img className="logo-background" src={logoVertical} alt="Logo" />
+                </div>
+
+            </section>
         </>
     )
 }
