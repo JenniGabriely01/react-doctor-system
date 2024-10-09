7c19e9a110639d3f8cc118978ed11182f9a1a741
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-10-09 14:17:23 -0300

    Adiciona sistema de emprestimo

--- src/pages/Prazos/prazos.jsx
+++ src/pages/Prazos/prazos.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import BarraSearch from "../../components/barraSearch/barraSearch";
 import MenuLateral from "../../components/menuLateral/menuLateral";
 import TituloGrande from "../../components/tituloGrande/tituloGrande";
 import './prazos.css'
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Prazos() {
+    const [emprestimos, setEmprestimos] = useState([]);
+
+    useEffect(() => {
+        // Buscar os empréstimos da API
+        axios.get('http://localhost:3000/api/emprestimos')
+            .then(response => {
+                setEmprestimos(response.data);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar empréstimos:', error);
+            });
+    }, []);
+
     return (
         <main className="prazos">
             <div>
@@ -18,6 +34,23 @@ export default function Prazos() {
                     <BarraSearch
                         placeholder="Pesquisar Clientes..." />
                 </header>
+
+                <div className="clientes-emprestimo">
+                    {emprestimos.map(emprestimo => (
+                        <li className="card-prazo" key={emprestimo._id}>
+                            <h3>{emprestimo.cliente.nome} {emprestimo.cliente.sobrenome}</h3>
+                            <p className="detalhes">Ver Detalhes</p>
+                            <p> <span className="emprestimo">Empréstimo:</span> {new Date(emprestimo.dataEmprestimo).toLocaleDateString()}</p>
+                            <p> <span className="entrega">Entrega:</span> 18/10/24 {/* temporario */}</p>
+                            {/* <ul>
+                            mostra livros
+                                {emprestimo.livros.map(livro => (
+                                    <li key={livro._id}>{livro.nomeLivro}</li>
+                                ))}
+                            </ul> */}
+                        </li>
+                    ))}
+                </div>
 
             </section>
         </main>
