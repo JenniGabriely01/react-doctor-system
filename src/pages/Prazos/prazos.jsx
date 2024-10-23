3bcdbe016f674e87e3bafe942b03eda793dd5c58
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-10-23 10:51:39 -0300

    Faz a rota prazos funcionar com os alerts e a barra de pesquisa

--- src/pages/Prazos/prazos.jsx
+++ src/pages/Prazos/prazos.jsx
@@ -62,44 +62,43 @@ export default function Prazos() {
 
     return (
         <>
-
             <ToastContainer />
             <main className="prazos">
                 <div>
                     <MenuLateral />
                 </div>
 
-            <section className="conteudo-prazos">
-                <header className="header-prazos">
-                    <TituloGrande tituloG="Prazos de Devolução" />
-                    <BarraSearch 
-                        placeholder="Pesquisar Clientes..." 
-                        onSearch={setSearchTerm} // Passa a função de atualização corretamente
-                    />
-                </header>
+                <section className="conteudo-prazos">
+                    <header className="header-prazos">
+                        <TituloGrande tituloG="Prazos de Devolução" />
+                        <BarraSearch
+                            placeholder="Pesquisar Clientes..."
+                            onSearch={setSearchTerm} // Passa a função de atualização corretamente
+                        />
+                    </header>
 
-                <div className="clientes-emprestimo">
-                    {filteredEmp.map(emprestimo => (
-                        <li className="card-prazo" key={emprestimo._id}>
-                            <h3>{emprestimo.cliente.nome} {emprestimo.cliente.sobrenome}</h3>
-                            <p className="detalhes" onClick={() => handleVerDetalhes(emprestimo._id)}>
-                                {expandedCard === emprestimo._id ? "Mostrar Menos" : "Ver Detalhes"}
-                            </p>
-                            <p><span className="emprestimo">Empréstimo:</span> {new Date(emprestimo.dataEmprestimo).toLocaleDateString()}</p>
-                            <p><span className="entrega">Entrega:</span> {calcularPrazoEntrega(emprestimo.dataEmprestimo)}</p>
+                    <div className="clientes-emprestimo">
+                        {filteredEmp.map(emprestimo => (
+                            <li className="card-prazo" key={emprestimo._id}>
+                                <h3>{emprestimo.cliente.nome} {emprestimo.cliente.sobrenome}</h3>
+                                <p className="detalhes" onClick={() => handleVerDetalhes(emprestimo._id)}>
+                                    {expandedCard === emprestimo._id ? "Mostrar Menos" : "Ver Detalhes"}
+                                </p>
+                                <p><span className="emprestimo">Empréstimo:</span> {new Date(emprestimo.dataEmprestimo).toLocaleDateString()}</p>
+                                <p><span className="entrega">Entrega:</span> {calcularPrazoEntrega(emprestimo.dataEmprestimo)}</p>
 
-                            {expandedCard === emprestimo._id && (
-                                <>
-                                    <hr />
-                                    <h4>Livros emprestados</h4>
-                                    <ul>
-                                        {emprestimo.livros.map(livro => (
-                                            <li key={livro._id}>{livro.nomeLivro}</li>
-                                        ))}
-                                    </ul>
-                                    <hr />
-                                    <div className="devolucao">
-                                        <h5>O(s) livro(s) foram devolvido(s)?</h5>
+                                {expandedCard === emprestimo._id && (
+                                    <>
+                                        <hr />
+                                        <h4>Livros emprestados</h4>
+                                        <ul>
+                                            {emprestimo.livros.map(livro => (
+                                                <li key={livro._id}>{livro.nomeLivro}</li>
+                                            ))}
+                                        </ul>
+                                        <hr />
+                                        <div className="devolucao">
+                                            <h5>O(s) livro(s) foram devolvido(s)?</h5>
 
                                             <div className="btn-escolha">
                                                 <button
@@ -112,18 +111,20 @@ export default function Prazos() {
                                                 </button>
                                             </div>
 
-                                    </div>
-                                </>
-                            )}
-                        </li>
-                    ))}
-                    {emprestimos.length > 6 && (
-                        <button onClick={() => setMostrarTodos(!mostrarTodos)}>
-                            {mostrarTodos ? "Mostrar menos" : "Ver mais"}
-                        </button>
-                    )}
-                </div>
-            </section>
-        </main>
+                                        </div>
+                                    </>
+                                )}
+                            </li>
+                        ))}
+                        {emprestimos.length > 6 && (
+                            <button onClick={() => setMostrarTodos(!mostrarTodos)}>
+                                {mostrarTodos ? "Mostrar menos" : "Ver mais"}
+                            </button>
+                        )}
+                    </div>
+                </section>
+            </main>
+        </>
+
     );
 }
