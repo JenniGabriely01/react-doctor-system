5f99eda3349bfb91fa8c630c3ca5b4291ca965b5
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-11-01 11:16:47 -0300

    Adiciona autores principais no Dashboard

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -7,29 +7,21 @@ import imgteste from '../../assets/imagens/banner.jpg';
 export default function Home() {
     const [generos, setGeneros] = useState([]);
     const [clientes, setClientes] = useState([]);
+    const [autores, setAutores] = useState([]);
+
+    const fetchData = async (url, setState) => {
+        try {
+            const response = await axios.get(url);
+            setState(response.data);
+        } catch (error) {
+            console.error(`Erro ao buscar dados de ${url}`);
+        }
+    };
 
     useEffect(() => {
-        const fetchGeneros = async () => {
-            try {
-                const response = await axios.get('http://localhost:3000/api/generos-principais');
-                setGeneros(response.data);
-            } catch (error) {
-                console.error("Erro ao buscar por gêneros");
-            }
-        };
-        fetchGeneros();
-    }, []);
-
-    useEffect(() => {
-        const fetchClientes = async () => {
-            try {
-                const response = await axios.get('http://localhost:3000/api/clientes-principais');
-                setClientes(response.data);
-            } catch (error) {
-                console.error("Erro ao buscar por clientes");
-            }
-        };
-        fetchClientes();
+        fetchData('http://localhost:3000/api/autores-principais', setAutores);
+        fetchData('http://localhost:3000/api/generos-principais', setGeneros);
+        fetchData('http://localhost:3000/api/clientes-principais', setClientes);
     }, []);
 
     return (
@@ -43,13 +35,14 @@ export default function Home() {
             <section className="conteudo-direita">
                 <div className="principais">
                     <h2>Autores principais</h2>
-                    {generos.map((genero) => (
-                        <div key={genero.posicao}>
-                            <h3>{genero.posicao}. {genero.genero}</h3>
-                            <p>{genero.totalEmprestimos} empréstimos</p>
+                    {autores.map((autor) => (
+                        <div key={autor.posicao}>
+                            <h3>{autor.posicao} {autor.autor}</h3>
+                            <p>{autor.totalEmprestimos} empréstimos</p>
                         </div>
                     ))}
                 </div>
+
                 <div className="principais">
                     <h2>Gêneros principais</h2>
                     {generos.map((genero) => (
