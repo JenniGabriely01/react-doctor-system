25171bf90500574eeaa7d5860b9ff754b51fcde8
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-11-06 14:39:52 -0300

    Adiciona conteudo dos carroseis do dashboard

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -3,12 +3,16 @@ import MenuLateral from "../../components/menuLateral/menuLateral";
 import './dashboard.css';
 import axios from "axios";
 import carrosel1 from '../../assets/imagens/carrosel1.png';
+import carrosel2 from '../../assets/imagens/carrosel2.png';
+import carrosel3 from '../../assets/imagens/carrosel3.png';
 import { Link } from "react-router-dom";
 
 export default function Home() {
     const [generos, setGeneros] = useState([]);
     const [clientes, setClientes] = useState([]);
     const [autores, setAutores] = useState([]);
+    const [emprestimosRecentes, setEmprestimosRecentes] = useState(0);
+    const [atrasosSemana, setAtrasosSemana] = useState(0);
 
     const fetchData = async (url, setState) => {
         try {
@@ -19,10 +23,32 @@ export default function Home() {
         }
     };
 
+    // Função para emprestimos recentes
+    const fecthEmprestimosRecentes = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/emprestimos/last-week-count');
+            setEmprestimosRecentes(response.data.count);
+        } catch {
+            console.error("Erro ao buscar contagem de empréstimos recentes:", error);
+        }
+    };
+
+    // Função que conta todos os atrasos
+    const fetchAtrasosTotal = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/emprestimos/atrasos-total');
+            setAtrasosSemana(response.data.count);
+        } catch (error) {
+            console.error("Erro ao buscar total de atrasos", error);
+        }
+    };
+
     useEffect(() => {
         fetchData('http://localhost:3000/api/autores-principais', setAutores);
         fetchData('http://localhost:3000/api/generos-principais', setGeneros);
         fetchData('http://localhost:3000/api/clientes-principais', setClientes);
+        fecthEmprestimosRecentes();
+        fetchAtrasosTotal();
     }, []);
 
     return (
@@ -36,6 +62,21 @@ export default function Home() {
                         <h2>Clientes cadastrados</h2>
                         <p>Dessa semana</p>
                         <Link to="/Clientes">Ver mais</Link>
+                    </div>
+                </div>
+                <div className="carrosel">
+                    <img src={carrosel2} alt="" />
+                    <div className="txt-carrosel emprestimoRecente">
+                        <h2>{emprestimosRecentes} Livro(s)</h2>
+                        <p>Emprestados essa semana</p>
+                    </div>
+                </div>
+                <div className="carrosel">
+                    <img src={carrosel3} alt="" />
+                    <div className="txt-carrosel">
+                        <h2>{atrasosSemana} Livro(s)</h2>
+                        <p>Atrasado(s)</p>
+                        <Link to="/prazos">Ver mais</Link>
                     </div>
                 </div>
             </section>
