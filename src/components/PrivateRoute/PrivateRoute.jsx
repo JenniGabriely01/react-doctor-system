6c5a33b86456c90c49a8a2cfcdd04eb01efb4e64
DevSesi2 <henrique.marchini.souza@gmail.com>
2024-11-22 11:20:22 -0300

    Token validado

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,17 +1,43 @@
-// src/components/PrivateRoute.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth'; 
 
 const PrivateRoute = ({ children }) => {
-    //const { isAuthenticated } = useAuth();
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
     const token = sessionStorage.getItem('token');
-     
-    if (token == null) {
-        return <Navigate to="/login" />;
+
+    useEffect(() => {
+        const validateToken = async () => {
+            if (token) {
+                try {
+                    const response = await fetch('http://localhost:3000/validate', {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    
+                    if (response.ok) {
+                        setIsAuthenticated(true);
+                    } else {
+                        setIsAuthenticated(false);
+                    }
+                } catch (error) {
+                    console.error('Erro ao validar token:', error);
+                    setIsAuthenticated(false);
+                }
+            } else {
+                setIsAuthenticated(false);
+            }
+        };
+
+        validateToken();
+    }, [token]);
+
+    if (isAuthenticated === null) {
+        return <div>Loading...</div>; // Ou um spinner de carregamento
     }
 
-    return children;
+    return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
